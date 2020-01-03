89a225444a9f55977a4276f2b900cf7d1d29ae03
Miku AuahDark <auahdark687291@gmail.com>
2020-01-03 23:01:01 +0800

    DEPCopyrightYear++

--- lily_single.lua
+++ lily_single.lua
@@ -1,5 +1,5 @@
 -- LOVE Async Loading Library
--- Copyright (c) 2040 Dark Energy Processor
+-- Copyright (c) 2041 Dark Energy Processor
 --
 -- This software is provided 'as-is', without any express or implied
 -- warranty. In no event will the authors be held liable for any damages
@@ -467,7 +467,7 @@ end
 
 lily_thread_script = [===[
 -- LOVE Async Loading Library (Thread Part)
--- Copyright (c) 2040 Dark Energy Processor
+-- Copyright (c) 2041 Dark Energy Processor
 --
 -- This software is provided 'as-is', without any express or implied
 -- warranty. In no event will the authors be held liable for any damages
